e68936ed09720e1784cd195a005d51086830d9b9
2SuryaPrakash <152375770+2SuryaPrakash@users.noreply.github.com>
2024-02-03 01:26:19 +0530

    .

---
+++ app/src/main/java/com/example/webarelyc/Student_login.java
@@ -0,0 +1,38 @@
+package com.example.webarelyc;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class Student_login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_student_login);
+        //StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        //StrictMode.setThreadPolicy(policy);
+
+
+
+        Button submit = (Button)findViewById(R.id.submit);
+
+
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText user = (EditText)findViewById(R.id.username);
+                EditText pass = (EditText)findViewById(R.id.password);
+                String u =user.getText().toString();
+                String p=pass.getText().toString();
+                Intent intent= new Intent(getApplicationContext(), studentOrderMenu.class);
+                startActivity(intent);
+            }
+        });
+
+
+    }
+}
